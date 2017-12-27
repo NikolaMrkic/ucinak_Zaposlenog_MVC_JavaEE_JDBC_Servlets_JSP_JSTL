790bc9c342e1635e74e3fec246fa3a76a74b9082
Nikola Mrkic <nikola.mrkic93@gmail.com>
2017-12-27 11:29:02 +0100

    dodavanje tima u projekat

---
+++ src/java/domen/Tim.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domen;
+
+/**
+ *
+ * @author nikol
+ */
+public class Tim {
+    
+    private int id,id_Zaposleni;
+    private double brSati;
+    private String  nazivTima;
+
+    public int getId() {
+        return id;
+    }
+
+    public int getId_Zaposleni() {
+        return id_Zaposleni;
+    }
+
+    public double getBrSati() {
+        return brSati;
+    }
+
+    public String getNazivTima() {
+        return nazivTima;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setId_Zaposleni(int id_Zaposleni) {
+        this.id_Zaposleni = id_Zaposleni;
+    }
+
+    public void setBrSati(double brSati) {
+        this.brSati = brSati;
+    }
+
+    public void setNazivTima(String nazivTima) {
+        this.nazivTima = nazivTima;
+    }
+
+    
+    
+}
